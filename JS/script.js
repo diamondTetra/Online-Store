7804411f009f0badb917ba5dfd9717a6cd736798
diamondTetra <diamondtetratech@gmail.com>
2023-07-04 12:31:14 +0200

    Changes

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,28 @@
+// Navigation
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar");
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+menuBtn.onclick = ()=>{
+    menu.classList.add("active");
+    menuBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+    body.classList.add("disabledScroll");
+}
+cancelBtn.onclick = ()=>{
+    menu.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+    body.classList.remove("disabledScroll");
+}
+
+window.onscroll = ()=>{
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+}
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
     const clientsParagraph = document.getElementById('clients');
     const displayButton = document.getElementById('displayButton');
