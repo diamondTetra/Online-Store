14738ce19808f0666fd5c79248547d5a2536867c
diamondTetra <diamondtetratech@gmail.com>
2023-07-05 18:09:08 +0200

    today

--- JS/script.js
+++ JS/script.js
@@ -22,7 +22,7 @@ window.onscroll = ()=>{
 }
 
 
-
+// store information on the text files
 window.addEventListener('DOMContentLoaded', () => {
     const clientsParagraph = document.getElementById('clients');
     const displayButton = document.getElementById('displayButton');
@@ -48,3 +48,46 @@ window.addEventListener('DOMContentLoaded', () => {
         .catch(error => console.log('Error:', error));
     });
 });
+
+// Slider
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+
